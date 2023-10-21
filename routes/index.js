35236ed527da9888c6c1b523b312e68a67e07223
petermirithu <peter_mirithu@yahoo.com>
2023-10-21 16:43:57 +0300

    change mail app name

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-    // res.render('pages/home');
-    res.render('pages/signIn');
+    res.render('pages/home');    
 });
 
 router.get('/signIn', function (req, res, next) {
