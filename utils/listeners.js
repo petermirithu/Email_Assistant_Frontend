ecf1841a4d00dbc6f41a9a5132042ae1a9caa31e
petermirithu <peter_mirithu@yahoo.com>
2023-11-15 08:14:06 +0300

    add functionality to make pdfs visible in the app.

--- utils/listeners.js
+++ utils/listeners.js
@@ -2,16 +2,20 @@ const subscriptionService = require('../services/subscription');
 const userService = require('../services/userService');
 const nodeMailSender = require('./mailSender');
 const moment = require('moment');
+const fs = require('fs');
+const pdf = require('pdf-parse');
 
 module.exports = function (io) {
     io.on('connection', (socket) => {
         let emails = []
         let tasks = [];
+        let attachments = [];
 
-        socket.on('fetch_mails', async () => {                        
+        socket.on('fetch_mails', async () => {
             await userService.fetchProcessedEmails().then(response => {
                 emails = response.data.emails;
                 tasks = response.data.tasks;
+                attachments = response.data.attachments;
 
                 for (let email of emails) {
                     const timeAgo = moment(new Date(email.created_at)).fromNow()
@@ -30,37 +34,84 @@ module.exports = function (io) {
                     const timeAgo = moment(new Date(task.created_at)).fromNow()
                     task["timeAgo"] = timeAgo;
                 }
-                io.emit("display_mails_tasks", {emails:emails, tasks:tasks});
+
+                for (let attachment of attachments) {
+                    attachment["extension"] = attachment.name.split(".")[1]
+
+                    const timeAgo = moment(new Date(attachment.created_at)).fromNow()
+                    attachment["timeAgo"] = timeAgo;
+                }
+
+                io.emit("display_mails_tasks", { emails: emails, tasks: tasks, attachments: attachments });
             }).catch(error => {
                 console.log(error)
+            });
+        });
+
+        socket.on('get_mail', async (emailId) => {
+            const email = emails.find(x => x?.id == emailId);
+            const emailTasks = tasks.filter(x => x?.email_id == emailId && x?.belongs_to == "email");
+            const emailAttachments = attachments.filter(x => x?.email_id == emailId) || [];
+
+            io.emit("display_mail_task", { email: email, tasks: emailTasks, attachments: emailAttachments});
+        });
+
+        socket.on('generate_reply_suggestion', async (emailDetailBody) => {
+            await userService.generateReplySuggestion({ emailBody: emailDetailBody }).then(response => {
+                io.emit("display_reply_suggestion", response.data);
+            }).catch(error => {
+                console.log(error)
+            });
+        });
+
+        socket.on('send_reply', async (payload) => {
+            nodeMailSender.sendMail(payload);
+        });
+
+        socket.on('generate_mail_summary', async (emailDetailBody) => {
+            await userService.generateMailSummary({ emailBody: emailDetailBody }).then(response => {
+                io.emit("display_mail_summary", response.data);
+            }).catch(error => {
+                console.log(error);
+            });
+        });
+
+        socket.on('get_attachment', async (attachmentId) => {
+            const attachment = attachments.find(x => x?.id == attachmentId);            
+            const email = emails.find(x => x?.id == attachment?.email_id);                        
+            const emailTasks = tasks.filter(x => x?.email_id == attachment?.email_id && x?.belongs_to == "attachment");                                
+            io.emit("display_attachment_task", { attachment: attachment, email: email, tasks: emailTasks});
+        });
+
+        socket.on('generate_attachment_summary', async (fileName) => {
+            const dataBuffer = fs.readFileSync("public/attachments/"+fileName);
+            // Convert the PDF buffer to text
+            await pdf(dataBuffer).then(async data => {
+                // Extracted text from the PDF                
+                await userService.generateMailSummary({ emailBody: data.text }).then(response => {
+                    io.emit("display_attachment_summary_reply", {text:response.data, option: "summary"});
+                }).catch(error => {
+                    console.log(error);
+                });
+            }).catch(error => {                
+                console.error(error);
             });            
         });
 
-        socket.on('get_mail', async (emailId) => {            
-            const email = emails.find(x=>x?.id == emailId);
-            const emailTasks = tasks.filter(x=>x?.email_id == emailId);
-            io.emit("display_mail_task", {email:email, tasks:emailTasks});
-        });
-
-        socket.on('generate_reply_suggestion', async (emailDetailBody) => {            
-            await userService.generateReplySuggestion({emailBody: emailDetailBody}).then(response=>{                
-                io.emit("display_reply_suggestion", response.data);
-            }).catch(error=>{
-                console.log(error)
-            });                        
-        });       
-        
-        socket.on('send_reply', async (payload) => {   
-            nodeMailSender.sendMail(payload);                                 
-        });        
-
-        socket.on('generate_mail_summary', async (emailDetailBody) => {   
-            await userService.generateMailSummary({emailBody: emailDetailBody}).then(response=>{                
-                io.emit("display_mail_summary", response.data);
-            }).catch(error=>{
-                console.log(error);
-            });                               
-        });                
+        socket.on('generate_attachment_reply_suggestion', async (fileName) => {
+            const dataBuffer = fs.readFileSync("public/attachments/"+fileName);
+            // Convert the PDF buffer to text
+            await pdf(dataBuffer).then(async data => {
+                // Extracted text from the PDF                
+                await userService.generateReplySuggestion({ attachmentBody: data.text }).then(response => {
+                    io.emit("display_attachment_summary_reply", {text:response.data, option: "reply"});
+                }).catch(error => {
+                    console.log(error);
+                });
+            }).catch(error => {                
+                console.error(error);
+            });            
+        });            
     });
 
     subscriptionService.showError$.subscribe(
@@ -90,7 +141,7 @@ module.exports = function (io) {
     subscriptionService.mailProcessed$.subscribe(
         (data) => {
             if (data != null) {
-                // ............................
+                io.emit("mail_processed", data);
                 data = null;
             }
         });
--- utils/mailListener.js
+++ utils/mailListener.js
@@ -2,6 +2,7 @@ var { MailListener } = require("mail-listener5");
 const subscriptionService = require('../services/subscription');
 const userService = require("../services/userService");
 const cacheService = require("../services/cacheService");
+const pdf = require('pdf-parse');
 
 class NodeMailListener {
     constructor() {
@@ -11,51 +12,52 @@ class NodeMailListener {
     }
 
     initListener = (email, password) => {
-        this.username=email,
-        this.password=password,
+        this.username = email,
+            this.password = password,
 
-        this.mailListener = new MailListener({
-            username: this.username,
-            password: this.password,            
-            host: "smtp.gmail.com",
-            port: 993,
-            tls: true,
-            connTimeout: 30000,
-            authTimeout: 10000,
-            keepalive: true,
-            // debug: null, // Or your custom function with only one incoming argument. Default: null
-            autotls: 'never', // default by node-imap
-            tlsOptions: { rejectUnauthorized: false },
-            mailbox: "INBOX", // mailbox to monitor
-            searchFilter: ["UNSEEN"], // the search filter being used after an IDLE notification has been retrieved
-            markSeen: true, // all fetched email will be marked as seen and not fetched next time
-            fetchUnreadOnStart: true, // use it only if you want to get all unread email on lib start. Default is `false`,
-            attachments: true, // download attachments as they are encountered to the project directory
-            attachmentOptions: { directory: "attachments/" } // specify a download directory for attachments
-        });
+            this.mailListener = new MailListener({
+                username: this.username,
+                password: this.password,
+                host: "smtp.gmail.com",
+                port: 993,
+                tls: true,
+                connTimeout: 30000,
+                authTimeout: 10000,
+                keepalive: true,
+                // debug: null, // Or your custom function with only one incoming argument. Default: null
+                autotls: 'never', // default by node-imap
+                tlsOptions: { rejectUnauthorized: false },
+                mailbox: "INBOX", // mailbox to monitor
+                searchFilter: ["UNSEEN"], // the search filter being used after an IDLE notification has been retrieved
+                markSeen: true, // all fetched email will be marked as seen and not fetched next time
+                fetchUnreadOnStart: true, // use it only if you want to get all unread email on lib start. Default is `false`,
+                attachments: true, // download attachments as they are encountered to the project directory
+                attachmentOptions: { directory: "public/attachments/" } // specify a download directory for attachments
+            });
 
-        this.mailListener.on("server:connected", function () {            
+        this.mailListener.on("server:connected", function () {
             subscriptionService.updateMailConnectionStatus("connected");
         });
 
-        this.mailListener.on("mailbox", function (mailbox) {              
+        this.mailListener.on("mailbox", function (mailbox) {
             subscriptionService.updateMailBoxTotal(mailbox.messages.total); // this field in mailbox gives the total number of emails                        
         });
 
-        this.mailListener.on("server:disconnected", function () {            
+        this.mailListener.on("server:disconnected", function () {
             subscriptionService.updateMailConnectionStatus("disconnected");
         });
 
         this.mailListener.on("error", function (err) {
+            console.log(err);
             subscriptionService.updateMailConnectionStatus("disconnected");
-            if (err?.textCode == "AUTHENTICATIONFAILED") {                
-                subscriptionService.setShowError("The password you entered for your email iMAP is wrong. Please correct your password.\nNo email tracking is happenning now!");                
+            if (err?.textCode == "AUTHENTICATIONFAILED") {
+                subscriptionService.setShowError("The password you entered for your email iMAP is wrong. Please correct your password.\nNo email tracking is happenning now!");
             }
-            else if(err?.code == "ETIMEDOUT"){                
-                subscriptionService.setShowError("Ooops! Stopped listening to emails due to in activity! Login again to verify that your still around.");                                
+            else if (err?.code == "ETIMEDOUT") {
+                subscriptionService.setShowError("Ooops! Stopped listening to emails due to in activity! Login again to verify that your still around.");
             }
-            else {                
-                subscriptionService.setShowError("Ooops! Something went wrong while trying to listen to your emails");                                
+            else {
+                subscriptionService.setShowError("Ooops! Something went wrong while trying to listen to your emails");
             }
         });
 
@@ -68,25 +70,37 @@ class NodeMailListener {
         })
 
         this.mailListener.on("attachment", function (attachment, path, seqno) {
-            // do something with attachment
+            // do something with attachment                                                
         });
 
         this.mailListener.on("mail", async function (mail, seqno) {
-            // do something with the whole email as a single object                                                
-            const userProfile = await cacheService.getUserData();
+            // do something with the whole email as a single object              
+            const userProfile = await cacheService.getUserData();            
 
             const payload = {
-                userId: userProfile?.id, 
-                subject: mail.subject, 
-                fromEmail: mail.from.text, 
-                body: mail.text,         
+                userId: userProfile?.id,
+                subject: mail.subject,
+                fromEmail: mail.from.text,
+                body: mail.text,
                 htmlBody: mail.textAsHtml,
-                messageId: mail.messageId     
-            }                        
+                messageId: mail.messageId,
+                attachments: []
+            }
+            
+            if (mail?.attachments?.length > 0) {
+                for (let attachment of mail?.attachments) {
+                    const pdfBuffer = Buffer.from(attachment.content, 'base64');
+                    await pdf(pdfBuffer).then(async (data) => {                        
+                        payload.attachments.push({fileName: attachment.filename, fileText: data.text});                        
+                    }).catch((error) => {
+                        console.error('Error extracting text from PDF:', error);
+                    });
+                }
+            }
 
-            await userService.processEmail(payload).then(response=>{                
+            await userService.processEmail(payload).then(response => {
                 subscriptionService.updateMailProcessed("fetchAll");
-            }).catch(error=>{                            
+            }).catch(error => {
                 subscriptionService.setShowError("Oops! Something went wrong while processing one of your email");
             });
         })
